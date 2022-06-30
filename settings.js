a7e631814bd680b6f3daecc95cd6cca1c5c0691f
whuzzzzzz <108463003+whuzzzzzz@users.noreply.github.com>
2022-06-30 16:21:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.owner = ['94781654650']
+global.premium = ['94781654650']
+global.ownernomer = '941654650'
+global.ownername = 'WHIZ✔'
+global.botname = 'BLAZE'
+global.footer = '©BLAZEInc.'
+global.ig = 'https://github.com'
+global.region = 'Sri Lanka'
+global.sc = 'https://github.com'
+global.myweb = 'https://youtube.com'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = 'WHIZ✔'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
